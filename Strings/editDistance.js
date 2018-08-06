a62ef5cb34a851c26d9ffddaf24285249c99ad8b
Jack Schofield <jjmschofield@googlemail.com>
2018-08-07 00:16:58 +0100

    Adding editDistance practice

---
+++ Strings/editDistance.js
@@ -0,0 +1,43 @@
+// There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character.
+// Given two strings, write a function to check if they are one edit (or zero edits) away.
+
+// pale, ple -> true
+// pales. pale -> true
+// pale. bale -> true
+// pale. bake -> false
+
+// Solution A) Check each letter, recursively going back and padding where a letter has been omitted O(N!)
+// Solution B) Create a map of count and positions and then calculate the difference O(A + B) - didn't work
+// Solution C) Iterate A, if no match score 1, if length of a is greater add skip, if length of a is less minus skip O(N)
+module.exports = {
+  isOneOrLessEditDistance: (strA, strB) => {
+    const stringDistance = strA.length - strB.length;
+
+    if(stringDistance > 1 || stringDistance < -1){
+      return false;
+    }
+
+    let score = 0;
+    let skip = 0;
+
+    for (let i = 0; i < strB.length; i++) {
+      let aIndex = i + skip;
+
+      if (aIndex >= strA.length) break;
+
+      if (strA[aIndex] === strB[i]) { continue; }
+
+      score++;
+
+      if (strA.length > strB.length) {
+        skip++;
+      }
+
+      if (strA.length < strB.length) {
+        skip--;
+      }
+    }
+
+    return score <= 1 && score >= -1;
+  }
+};
---
+++ Strings/editDistance.test.js
@@ -0,0 +1,40 @@
+const { isOneOrLessEditDistance } = require('./editDistance');
+
+describe('Strings', function () {
+  describe('editDistance', function () {
+    describe('isOneOrLessEditDistance', function () {
+      it('should return true when string b is missing a character in the middle', function () {
+        expect(isOneOrLessEditDistance("pale", "ple")).toEqual(true);
+      });
+
+      it('should return true when given when string b i missing a chracter at the end', function () {
+        expect(isOneOrLessEditDistance("pales", "pale")).toEqual(true);
+      });
+
+      it('should return true when when string b has one changed character', function () {
+        expect(isOneOrLessEditDistance("pale", "bale")).toEqual(true);
+      });
+
+      it('should return true when when string b has an extra character at the start', function () {
+        expect(isOneOrLessEditDistance("ale", "bale")).toEqual(true);
+      });
+
+      it('should return false when string b has two changed characters', function () {
+        expect(isOneOrLessEditDistance("pale", "bake")).toEqual(false);
+      });
+
+      it('should return false when string a is two longer then string b', function () {
+        expect(isOneOrLessEditDistance("pale", "pa")).toEqual(false);
+      });
+
+      it('should return false when string b is two longer then string a', function () {
+        expect(isOneOrLessEditDistance("pa", "pale")).toEqual(false);
+      });
+    });
+  });
+});
+
+// pale, ple -> true
+// pales. pale -> true
+// pale. bale -> true
+// pale. bake -> false
